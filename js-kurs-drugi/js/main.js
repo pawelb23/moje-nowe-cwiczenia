fc452ea4fd1b601bf2b79bf887a2460284d9aca1
Paweł Białek <pawelbialek23@op.pl>
2018-04-28 01:55:40 +0200

    Dwie funkcje z pełnymi komentarzami

--- js-kurs-drugi/js/main.js
+++ js-kurs-drugi/js/main.js
@@ -1,11 +1,56 @@
 'use strict';
 
-console.log('czy działa');
+//1>>>SPOSÓB<<<
 
-function tableAverage(arr) {
+function tableAverage(arrJeden) {
     let sum = 0;
-    for (let i=0; i<arr.length; i++) {
-        sum += arr[i];
+    for (let i=0; i<arrJeden.length; i++) {
+//arrJeden.length --- pokaże w console.log ilość elementów (ogółem --- czyli cyfrę 7), zaś arrJeden[i] pokaże w console.log kolejne cyfry z tabeli (czyli --- 1,2,3,4,5,6,7).  
+        
+//        console.log(arrJeden[i]);
+        sum += arrJeden[i];
+//        console.log(sum);
     }
-    return (sum / i);
+    var result = (sum / arrJeden.length);
+       
+    console.log('Ilość elementów w tabeli --- ' + arrJeden.length);
+    
+//     console.log(result);
+    
+    return result;  
 }
+
+var tableNumbers = [1,2,3,4,5,6,7,8,9];
+
+console.log('Wynik --- ' + tableAverage(tableNumbers));
+
+console.log('==========================');
+//==========================================
+
+//2>>>SPOSÓB<<<
+
+function tableAverageDwa(arrDwa) {
+    let sum = 0;
+    for (let j=0; j<arrDwa.length; j++) {
+//arrDwa.length --- pokaże w console.log ilość elementów (ogółem --- czyli cyfrę 7), zaś arrDwa[j] pokaże w console.log kolejne cyfry z tabeli (czyli --- 1,2,3,4,5,6,7).  
+        
+//        console.log(arrDwa[j]);
+        sum += arrDwa[j];
+//        console.log(sum);
+        
+        var inter = arrDwa[j];//aby system odczytał musimy arrDwa[i] przyrównać do zmiennej
+    }
+    var resultDwa = (sum / inter);
+    
+       console.log('Ilość elementów w tabeli --- ' + inter);
+    
+//     console.log(result);
+    
+    return resultDwa;  
+}
+
+var arrDwa = [1,2,3,4,5,6,7,8];// nazwa taka sama jak parametru w funkcji (na kursie uczyli mnie zdecydowanie pierwszego sposobu)
+
+console.log('Wynik --- ' + tableAverageDwa(arrDwa));
+
+console.log('==========================');
