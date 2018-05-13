af7807b73970230d1c98a5eb47de14d56f47ee3a
Paweł Białek <pawelbialek23@op.pl>
2018-05-13 02:15:22 +0200

    poprawione komentarze

--- js-cwiczenia/js-kurs-drugi/js/main.js
+++ js-cwiczenia/js-kurs-drugi/js/main.js
@@ -5,7 +5,7 @@
 function tableAverage(arrJeden) {
     var sum = 0;
     for (var i=0; i<arrJeden.length; i++) {
-//arrJeden.length --- pokaże w console.log ilość elementów (ogółem --- czyli cyfrę 7), zaś arrJeden[i] pokaże w console.log kolejne cyfry z tabeli (czyli --- 1,2,3,4,5,6,7).  
+//arrJeden.length --- pokaże w console.log ilość elementów (ogółem --- czyli cyfrę 9), zaś arrJeden[i] pokaże w console.log kolejne cyfry z tabeli (czyli --- 1,2,3,4,5,6,7,8,9).  
         
 //        console.log(arrJeden[i]);
         sum += arrJeden[i];
@@ -32,19 +32,20 @@ console.log('==========================');
 function tableAverageDwa(arrDwa) {
     var sum = 0;
     for (var j=0; j<arrDwa.length; j++) {
-//arrDwa.length --- pokaże w console.log ilość elementów (ogółem --- czyli cyfrę 7), zaś arrDwa[j] pokaże w console.log kolejne cyfry z tabeli (czyli --- 1,2,3,4,5,6,7).  
+//arrDwa.length --- pokaże w console.log ilość elementów (ogółem --- czyli cyfrę 8), zaś arrDwa[j] pokaże w console.log kolejne cyfry z tabeli (czyli --- 1,2,3,4,5,6,7,8).  
         
 //        console.log(arrDwa[j]);
         sum += arrDwa[j];
 //        console.log(sum);
         
-        var inter = arrDwa[j];//aby system odczytał musimy arrDwa[i] przyrównać do zmiennej
+        var inter = arrDwa[j];//aby system odczytał arrDwa[j] (jako cyfrę 8 --- czyli 8 elementów) musimy arrDwa[j] przyrównać do zmiennej
+//        console.log (inter);
     }
     var resultDwa = (sum / inter);
     
        console.log('Ilość elementów w tabeli --- ' + inter);
     
-//     console.log(result);
+//     console.log(resultDwa);
     
     return resultDwa;  
 }
