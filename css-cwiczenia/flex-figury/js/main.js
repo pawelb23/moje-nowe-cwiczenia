95801aa389e8890c80ae4412ba3e1882e84920ac
Paweł Białek <pawelbialek23@op.pl>
2018-05-30 20:34:56 +0200

    zmiana koloru border wewnątrz

--- css-cwiczenia/flex-figury/js/main.js
+++ css-cwiczenia/flex-figury/js/main.js
@@ -1,3 +1,9 @@
 'use strict';
 
 console.log ('Figury z użyciem flex-a');
+
+var docHeight = document.documentElement.offsetHeight;//wysokość całego dokumentu
+console.log(docHeight);
+
+var docWidth = document.documentElement.offsetWidth;//szerokość całego dokumentu
+console.log(docWidth);
